03306dfa2a860cb3646a6b645786fc64dda131f9
Chris Clarizio <cclarizio@adobe.com>
2024-07-29 14:12:47 -0700

    Reorganize into WebExtension structure

---
+++ extension/content_scripts/main.js
@@ -0,0 +1,9 @@
+console.log("[vizsla] WebExtension loaded");
+
+document.body.style.border = "5px solid green";
+
+// import init, { greet } from "./vizsla/pkg/vizsla.js";
+//       init().then(() => {
+//         greet("Vizsla");
+//       });
+
