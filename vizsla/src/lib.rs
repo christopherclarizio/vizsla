3ed5abdf377601e40ba18598223e17ce63aad875
Chris Clarizio <cclarizio@adobe.com>
2024-09-16 13:20:17 -0700

    Add keydown event handler

--- vizsla/src/lib.rs
+++ vizsla/src/lib.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -13,6 +15,32 @@ extern "C" {
 
 //     #[wasm_bindgen(js_namespace=console)]
 //     fn error(s: &str);
+}
+
+// state: fn(input) -> state
+// normal_mode()
+
+fn buffer_input(buffered_input: vec<char>, event: web_sys::KeyboardEvent) -> Vec<char> {
+
+}
+
+extract_command(buffered_input: vec<char>, current_state: State) -> Option<Command> {
+
+}
+
+apply_command(command: Command, current_state: State) {
+
+}
+
+(":wq", InsertMode) -> fn write_and_quit(state: State) -> State { }
+
+fn handle_keydown_event(event: web_sys::KeyboardEvent) {
+    let buffered_input = buffer_input(buffered_input, event);
+
+    match extract_command(buffered_input, current_state) {
+        Some(command) => apply_command(current_state, command),
+        None => _
+    }
 }
 
 // Called when the wasm module is instantiated
@@ -31,12 +59,12 @@ fn main() -> Result<(), JsValue> {
     
     let _ = document.body().unwrap().style().set_property("border", "5px solid blue");
 
-    let closure = Closure::wrap(Box::new(move |event: web_sys::Event| {
+    let closure = Closure::wrap(Box::new(move |event: web_sys::KeyboardEvent| {
         // Your Rust function logic goes here
-        log(&format!("Event triggered: {:?}", event));
+        log(&format!("Event triggered: {:?}", event.key()));
     }) as Box<dyn FnMut(_)>); 
 
-    let _ = document.add_event_listener_with_callback("keypress", closure.as_ref().unchecked_ref()).expect("unable to register event listener");
+    let _ = document.add_event_listener_with_callback("keydown", closure.as_ref().unchecked_ref()).expect("unable to register event listener");
 
     log("[vizsla] wasm module starting");
 
